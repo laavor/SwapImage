0b3636e979223c9c8202e237ba7648d6eb074a43
laavor <39971288+laavor@users.noreply.github.com>
2019-08-25 16:36:50 -0300

    Version 1.0.0
    Fisrt Version

---
+++ README.txt
@@ -0,0 +1,6 @@
+UI Java Script to give productivity to your code.
+
+www.laavor.com
+Contact: support@laavor.com
+
+OBS: It is not necessary to use JQuery.
---
+++ SwapImage_1_0_0.js
@@ -0,0 +1,69 @@
+var laavorSIKarenGillan;var laavorSISophieTurner;function LaavorSwap(_jadsj_Barbour,Bouchard_27_kla0ga9){if(laavorSIKarenGillan===undefined||laavorSIKarenGillan===null)
+{laavorSIKarenGillan=[];}
+if(laavorSISophieTurner===undefined||laavorSISophieTurner===null)
+{laavorSISophieTurner=[];}
+if(Bouchard_27_kla0ga9===undefined||Bouchard_27_kla0ga9===null)
+{laavorSISophieTurner[_jadsj_Barbour]=Frey().OnSwap;}
+else
+{laavorSISophieTurner[_jadsj_Barbour]=Bouchard_27_kla0ga9.OnSwap;}
+Reggie(_jadsj_Barbour)}
+function Reggie(_sd_s_Ernest){var _Amy_Winchester_=document.getElementById(_sd_s_Ernest);laavorSIKarenGillan[_sd_s_Ernest]=[];if(_Amy_Winchester_!==undefined&&_Amy_Winchester_!==null){var _Dorcas_LH=_Amy_Winchester_.children;var Liz__Florabelle_=_Dorcas_LH.length;var _Armand_=_Amy_Winchester_.getAttribute("imageWidth");var _Gregory_=_Amy_Winchester_.getAttribute("imageHeight");try{for(var _Martin_Keep_W=0;_Martin_Keep_W<Liz__Florabelle_;_Martin_Keep_W++){var Sessions_Steel;var Nicholas;if(_Martin_Keep_W===0)
+{Nicholas=true;}
+else
+{Nicholas=false;}
+var _il_Hilda=_Dorcas_LH[_Martin_Keep_W].getAttribute("laavorValue");Sessions_Steel={Joseph:_Dorcas_LH[_Martin_Keep_W],Heckl:_Martin_Keep_W,Carl:Nicholas,Kinsolving:_il_Hilda};if(_Armand_!==undefined&&_Armand_!==null&&_Armand_!=="")
+{_Dorcas_LH[_Martin_Keep_W].style.width=_Armand_.replace("px","")+'px';}
+if(_Gregory_!==undefined&&_Gregory_!==null&&_Gregory_!=="")
+{_Dorcas_LH[_Martin_Keep_W].style.height=_Gregory_.replace("px","")+'px';}
+if(_Martin_Keep_W!==0)
+{_Dorcas_LH[_Martin_Keep_W].style.display='none';}
+_Dorcas_LH[_Martin_Keep_W].setAttribute("Windsor",_sd_s_Ernest);_Dorcas_LH[_Martin_Keep_W].setAttribute("George",_Martin_Keep_W);_Dorcas_LH[_Martin_Keep_W].style.cursor="grab";laavorSIKarenGillan[_sd_s_Ernest].push(Sessions_Steel);_Dorcas_LH[_Martin_Keep_W].addEventListener("click",Gertrude);}}
+catch(e){throw new Error("The Base Content and Images order can not be interrupted, if necessary contact Laavor support.");}}}
+String.prototype.laavorToInteger=function(){"use strict";var returnItem=parseInt(this);return returnItem;};function Gertrude(event)
+{"use strict";var Raoul=event.currentTarget;var Eugene=Raoul.getAttribute("George").toString().laavorToInteger();var garrisonRoad=Raoul.getAttribute("Windsor").toString();var OldTown;var NewTown;var Daisy;Raoul.style.display='none';laavorSIKarenGillan[garrisonRoad][Eugene].Carl=false;if((Eugene+1)<laavorSIKarenGillan[garrisonRoad].length)
+{laavorSIKarenGillan[garrisonRoad][(Eugene+1)].Joseph.style.display='block';laavorSIKarenGillan[garrisonRoad][(Eugene+1)].Carl=true;OldTown=laavorSIKarenGillan[garrisonRoad][(Eugene+1)].Kinsolving;NewTown=laavorSIKarenGillan[garrisonRoad][(Eugene+1)].Eugene;Daisy=laavorSIKarenGillan[garrisonRoad][(Eugene+1)].Joseph.src;}
+else
+{laavorSIKarenGillan[garrisonRoad][0].Joseph.style.display='block';laavorSIKarenGillan[garrisonRoad][0].Carl=true;OldTown=laavorSIKarenGillan[garrisonRoad][0].Kinsolving;NewTown=laavorSIKarenGillan[garrisonRoad][0].Heckl;Daisy=laavorSIKarenGillan[garrisonRoad][0].Joseph.src;}
+if(laavorSISophieTurner!==undefined&&laavorSISophieTurner!==null)
+{if(laavorSISophieTurner[garrisonRoad]!==undefined&&laavorSISophieTurner[garrisonRoad]!==null)
+{try
+{laavorSISophieTurner[garrisonRoad](OldTown,NewTown,Daisy);}
+catch(errorLaavor)
+{try
+{laavorSISophieTurner[garrisonRoad](errorLaavor);}
+catch(errorLaavorI)
+{}}}}}
+function laavorGetValueSwapImageChosen(burbank)
+{if(laavorSIKarenGillan!==undefined&&laavorSIKarenGillan!==null)
+{if(laavorSIKarenGillan[burbank]!==undefined&&laavorSIKarenGillan[burbank]!==null)
+{for(var Godfrey=0;Godfrey<laavorSIKarenGillan[burbank].length;Godfrey++)
+{if(laavorSIKarenGillan[burbank][Godfrey].Carl.toString()==="true")
+{return laavorSIKarenGillan[burbank][Godfrey].Kinsolving;}}}}
+return"";}
+function laavorGetIndexSwapImageChosen(Jacques)
+{if(laavorSIKarenGillan!==undefined&&laavorSIKarenGillan!==null)
+{if(laavorSIKarenGillan[Jacques]!==undefined&&laavorSIKarenGillan[Jacques]!==null)
+{for(var Lucy=0;Lucy<laavorSIKarenGillan[Jacques].length;Lucy++)
+{if(laavorSIKarenGillan[Jacques][Lucy].Carl.toString()==="true")
+{return laavorSIKarenGillan[Jacques][Lucy].Heckl;}}}}
+return"";}
+function laavorGetSourceSwapImageChosen(May)
+{if(laavorSIKarenGillan!==undefined&&laavorSIKarenGillan!==null)
+{if(laavorSIKarenGillan[May]!==undefined&&laavorSIKarenGillan[May]!==null)
+{for(var Quaker=0;Quaker<laavorSIKarenGillan[May].length;Quaker++)
+{if(laavorSIKarenGillan[May][Quaker].Carl.toString()==="true")
+{return laavorSIKarenGillan[May][Quaker].Joseph.src;}}}}
+return"";}
+function laavorBlockSwapImage(Satsuma)
+{if(laavorSIKarenGillan!==undefined&&laavorSIKarenGillan!==null)
+{if(laavorSIKarenGillan[Satsuma]!==undefined&&laavorSIKarenGillan[Satsuma]!==null)
+{for(var Saigo=0;Saigo<laavorSIKarenGillan[Satsuma].length;Saigo++)
+{laavorSIKarenGillan[Satsuma][Saigo].Joseph.removeEventListener("click",Gertrude);}}}
+return"";}
+function laavorReleaseSwapImage(Leon)
+{if(laavorSIKarenGillan!==undefined&&laavorSIKarenGillan!==null)
+{if(laavorSIKarenGillan[Leon]!==undefined&&laavorSIKarenGillan[Leon]!==null)
+{for(var paul=0;paul<laavorSIKarenGillan[Leon].length;paul++)
+{laavorSIKarenGillan[Leon][paul].Joseph.addEventListener("click",Gertrude);}}}
+return"";}
+function Frey(){return{OnSwap:''};}
